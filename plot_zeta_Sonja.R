c5c03236080e308ddfa8415729423af6dde88b0e
soniapetrini <soniajpetrini@gmail.com>
2021-10-13 17:20:57 +0200

    zeta plot

--- plot_zeta_Sonja.R
+++ plot_zeta_Sonja.R
@@ -1,39 +1,54 @@
-zeta_d <- function(xvals,gamma){
-  return(1/(zeta(gamma)*xvals^gamma)); # definition of the zeta-function
-}
 
-# plot a zeta function with 'gamma' up to a maximal x-value of 'maxdegree'
-estimated_dist_zeta <- function(gamma,x){
-  xvals <- sort(unique(x),decreasing = F)
-  yvals <-zeta_d(xvals,gamma)
-  return(yvals)
-}
-
+# REAL DISTR
 # plot the real degree distribution of the data 
 real_dist <- function(x){
   degree_spectrum <- table(x)
   N <- length(x)
   degree_dist <- data.frame(degree_spectrum/N)
-  return(degree_dist$Freq)
+  return(degree_dist)
 }
 
+
+# ZETA
+# plot a zeta function with 'gamma'
+estimated_dist_zeta <- function(xvals,gamma){
+  return(1/(zeta(gamma)*xvals^gamma)); # definition of the zeta-function
+}
+
+# TRUNC ZETA
+H <- function(kmax,gamma) {
+  s <- 0
+  for (i in 1:kmax) {
+    s <- s + (1/(i^gamma))
+  }
+  return(s)
+}
+
+zeta_trunc_dist <- function(xvals,gamma,kmax){
+  return(1/(H(kmax,gamma)*xvals^gamma)); # definition of the zeta-trunc-function
+}
+
+
+
+
+
+
 # example usage (for Hungarian):
-x <- unlist(degree_distrib_ls[[8]])
+x <- unlist(degree_distrib_ls[[10]])
 xvals <- sort(as.integer(unique(x)))
+
 y_zeta <- estimated_dist_zeta(gamma = params_table$gamma_1[params_table$lang == "Hungarian"],
-                              x = x)
+                              xvals = xvals)
 y_real <- real_dist(x)
 
-plot(y_zeta)
-plot(y_real)
 
-dist_zeta <- data.frame("y_pred"= y_zeta, "y_real" = y_real, "degree" = xvals)
+dist_zeta <- data.frame("y_pred"= y_zeta, "y_real" = y_real$Freq, "degree" = as.integer(y_real$x))
 
 ggplot(data=dist_zeta) +
   geom_bar(aes(x=degree, y=y_pred),stat="identity", fill = "magenta") +
-  geom_bar(aes(x=degree, y=y_real),stat="identity", fill = "magenta") +
-  geom_point(aes(x=degree, y=y_pred),colour = "black", fill = "white") +
-  geom_point(aes(x=degree, y=y_real),colour = "black", fill = "white") +
+  geom_bar(aes(x=degree, y=y_real),stat="identity", fill = "green") +
+  geom_point(aes(x=degree, y=y_pred), colour = "red") +
+  geom_point(aes(x=degree, y=y_real), colour = "green") +
   theme_minimal() +
   labs(y= "freq",x="k") +
   scale_x_log10() +
@@ -41,4 +56,26 @@ ggplot(data=dist_zeta) +
   
 
 library(MLmetrics)
-MSE(y_zeta*N,y_real*N)
+MSE(y_zeta*N,y_real$Freq*N)
+
+
+
+
+
+y_zeta_trunc <- zeta_trunc_dist(xvals,
+                                gamma = params_table$gamma_2[params_table$lang == "Hungarian"],
+                                kmax = params_table$kmax[params_table$lang == "Hungarian"])
+y_real <- real_dist(x)
+
+dist_zeta_trunc <- data.frame("y_pred"= y_zeta_trunc, "y_real" = y_real$Freq, "degree" = as.integer(y_real$x))
+
+ggplot(data=dist_zeta_trunc) +
+  geom_bar(aes(x=degree, y=y_pred),stat="identity", fill = "magenta") +
+  geom_bar(aes(x=degree, y=y_real),stat="identity", fill = "green") +
+  geom_point(aes(x=degree, y=y_pred), colour = "red") +
+  geom_point(aes(x=degree, y=y_real), colour = "green") +
+  theme_minimal() +
+  labs(y= "freq",x="k") +
+  scale_x_log10() +
+  scale_y_log10()
+
